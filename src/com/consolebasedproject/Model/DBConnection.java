3d02af181ec8cd9f6177ba28d2500e9211b306e2
Anusuya T <108138000+anusuyathangaraj05@users.noreply.github.com>
2024-07-07 14:07:09 +0530

    updated database connection

--- src/com/consolebasedproject/Model/DBConnection.java
+++ src/com/consolebasedproject/Model/DBConnection.java
@@ -7,15 +7,19 @@ public class DBConnection {
     public static String url = "jdbc:mysql://localhost:3306/cproject";
     public static String user = "root";
     public static String password = "Anusuya_1234";
-
-    public static Connection provideConnection() {
-        Connection connect = null;
+    private static Connection con;
+    private DBConnection() {
         try {
             connect = DriverManager.getConnection(url, user, password);
         }
         catch (SQLException ex) {
             ex.fillInStackTrace();
         }
-        return connect;
+    }
+    public static void connect() {
+        new DBConnection();
+    }
+    public static Connection provideConnection() {
+        return DBConnection.con;
     }
 }
